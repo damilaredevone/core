b6ff0e01f65271200640350efb8162b6f57439e6
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-02-25 23:42:51 +0100

    chore: init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+ title: '@damilaredev/react-components',
+ themeConfig: {
+  editLinks: true,
+  editLinkText: 'Suggest changes to this page',
+  docsDir: 'docs',
+  docsBranch: 'main',
+  nav: [],
+  sidebar: {},
+ },
+});
---
+++ docs/index.md
@@ -0,0 +1,1 @@
+# @Damilaredev/Components
